f716af8480e346f7f691e24b02abbf86e38d8f97
unknown <saradawsone@gmail.com>
2022-05-31 08:22:20 -0700

    select and checkbox onchange working

--- src/components/Orderform.js
+++ src/components/Orderform.js
@@ -13,6 +13,17 @@ const initialFormValues = {
 const Orderform = () => {
     const [form, setForm] = useState(initialFormValues);
 
+    const formChange = (evt) => {
+        const name = evt.target.name;
+        const value = evt.target.type === 'checkbox' ? evt.target.checked : evt.target.value;
+
+        setForm({...form, [name]: value});
+    }
+
+    useEffect(() => {
+        console.log(form);
+    }, [form])
+
     return(
         <div id='orderform'>
             <h1 id="order-form-h2">Create Your Own Pizza!</h1>
@@ -24,13 +35,14 @@ const Orderform = () => {
                             name='fullname'
                             value={form.fullname}
                             type='text'
+                            onChange={formChange}
                         />
                     </label>
-                    <select id='size-dropdown'>
-                        <option>--Pick a Size--</option>
-                        <option>Small</option>
-                        <option>Medium</option>
-                        <option>Large</option>
+                    <select name='size' id='size-dropdown' onChange={formChange}>
+                        <option name='' value=''>--Pick a Size--</option>
+                        <option name='small' value='small'>Small</option>
+                        <option value='medium'>Medium</option>
+                        <option value='large'>Large</option>
                     </select>
                     <label> 
                         <p>Pick a Topping!</p>
@@ -39,24 +51,28 @@ const Orderform = () => {
                             type='checkbox'
                             name='peperoni'
                             checked={form.peperoni}
+                            onChange={formChange}
                         />
                         Olives
                         <input
                             type='checkbox'
                             name='olives'
                             checked={form.olives}
+                            onChange={formChange}
                         />
                         Peppers
                         <input
                             type='checkbox'
                             name='peppers'
                             checked={form.peppers}
+                            onChange={formChange}
                         />
                         Mushrooms
                         <input
                             type='checkbox'
                             name='mushrooms'
                             checked={form.mushrooms}
+                            onChange={formChange}
                         />
                     </label>
                     <label>
@@ -66,6 +82,7 @@ const Orderform = () => {
                             name='instructions'
                             value={form.instructions}
                             id='special-text'
+                            onChange={formChange}
                         />
                     </label>
                     <button id='order-button' type='submit'>
